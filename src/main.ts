5fe1f0b2696039c610ad299b3497a2505d92b354
bwv-hieu-ndt <hieu.ndt@briswell-vn.com>
2024-12-04 17:07:39 +0700

    feature: admin lte

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import express from 'express';
 import UserRoute from './api/user/user_route';
 import path from "path";
-
+import cors from "cors"
 
 const boostrap = async () => {
 
   const app = express();
   const port = 8080;
+  
 
   app.use(express.json());
+  app.use(cors({ origin: '*' }));
+
 
   const viewPath = path.join(__dirname, "views")
   const indexPath = path.join(__dirname, "views/index.html")
+  const appjs = path.join(__dirname, "views/app.js") 
+
+  app.use(express.static(appjs));
 
   app.set("view engine", "ejs");
   app.set("views", viewPath);
@@ -25,6 +31,7 @@ const boostrap = async () => {
   app.listen(port, () => {
     console.log(viewPath)
     console.log(indexPath)
+    console.log(appjs)
 
     console.log(`Server is running on port ${port}`)
   })
