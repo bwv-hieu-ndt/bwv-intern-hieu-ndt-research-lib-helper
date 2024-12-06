5ed2557799b2532b5c0d4308391b46a265d2ba77
bwv-hieu-ndt <hieu.ndt@briswell-vn.com>
2024-12-06 10:25:31 +0700

    Class-validator Adminlte Jqurey-validation regex

--- admin_lte/validation/jqueryValidation.js
+++ admin_lte/validation/jqueryValidation.js
@@ -1,3 +1,17 @@
+const regex_rules =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/;
+
+// function validateStrings(string) {
+//   return $.trim(string).match(regex_rules) ? true : false;
+//
+$.validator.addMethod(
+  "regex",
+  function (value, element, regex) {
+    return this.optional(element) || regex.test(value);
+  },
+  "Please enter a valid password."
+);
+
 $().ready(function () {
   $("#demoForm").validate({
     onfocusout: false,
@@ -11,10 +25,17 @@ $().ready(function () {
       password: {
         required: true,
         minlength: 8,
+        regex: regex_rules,
       },
       "re-password": {
         equalTo: "#password",
         minlength: 8,
+      },
+    },
+    messages: {
+      password: {
+        regex:
+          "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character, and be 8-10 characters long.",
       },
     },
   });
